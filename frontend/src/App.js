2bd27f2d7e215705f445d1e29da26e27eebf3b09
Davidmarla <david_martla@hotmail.com>
2023-02-09 16:57:52 +0100

    test de merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
-<<<<<<< HEAD
 
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
@@ -11,14 +10,11 @@ import { RegisterPage } from "./pages/RegisterPage";
 import { LoginPage } from "./pages/LoginPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { UserPage } from "./pages/UserPage";
-=======
 import PostList from "./components/PostList/PostList";
->>>>>>> origin/produccion
 
 function App() {
   return (
     <main>
-<<<<<<< HEAD
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -28,9 +24,7 @@ function App() {
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
-=======
       <PostList />
->>>>>>> origin/produccion
     </main>
   );
 }
