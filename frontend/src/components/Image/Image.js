346d3e4a36b50c55ac7fed7609ae3e48fa358cfb
SiTeuDic <siteudic@gmail.com>
2023-02-09 16:53:57 +0100

    intento solucion conflico 3

---
+++ frontend/src/components/Image/Image.js
@@ -0,0 +1,13 @@
+import "./Image.css";
+
+const Image = () => {
+  return (
+    <img
+      className="imgPost"
+      src="https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg"
+      alt=""
+    />
+  );
+};
+
+export default Image;
