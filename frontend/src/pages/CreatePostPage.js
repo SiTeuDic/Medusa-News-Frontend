f23d643d778a50853142769391ff10c2e647f2aa
SiTeuDic <siteudic@gmail.com>
2023-03-05 21:53:41 +0100

    la pagina de crear noticia te manda al login si no estas logeado

--- frontend/src/pages/CreatePostPage.js
+++ frontend/src/pages/CreatePostPage.js
@@ -1,6 +1,14 @@
 import Title from "../components/Title/Title";
 import CreatePost from "../components/CreatePost/CreatePost";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 const CreatePostPage = () => {
+  const user = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user.token) navigate("/login");
+  }, [navigate, user]);
   return (
     <section className="createPostSection">
       <Title text={"¿Qué está pasando?"} />
